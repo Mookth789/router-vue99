96c0fedda734694e9aa93543fa94f6642d89f3ad
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-04 12:56:46 +0700

    3 files modified, 3 added, 2 deleted

--- src/router.js
+++ src/router.js
@@ -11,23 +11,40 @@ const router = [
     }
   },
   {
-   path: '/login'    
-   name: 'Login',
-   component: () => import('./views/Login.vue'),
+
+    path: '/login',  
+    name: 'Login',
+    component: () => import('./views/Login.vue')
+
   },
+
   {
-  path: '/register',
-  name: 'Register',
-  component: () => import('./views/Register.vue'),
+    path: '/register',
+    name: 'Register',
+    component: () => import('./views/Register.vue')
   }
 ]
-
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
 
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    //Authentication check
+    const token = localStorage.getItem('token')
 
+    if (token) {
+      // Chech if token is valid
+      
+        return next()
+      }
+      
+      return next('/login')
+  }
+  next()
+
+})
 
 export default router
